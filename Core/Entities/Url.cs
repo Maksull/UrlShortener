c57273d9ba0b944c1269834c98df63e964f60d37
Maksull <kyrkyrkyr99@gmail.com>
2024-02-01 20:25:27 +0200

    Add basic entities

---
+++ Core/Entities/Url.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities;
+
+public sealed class Url
+{
+    public required long Id { get; set; }
+    public required string OriginalUrl { get; set; }
+    public required string ShortenedUrl { get; set; }
+    public required DateTime CreatedAt { get; set; }
+}
