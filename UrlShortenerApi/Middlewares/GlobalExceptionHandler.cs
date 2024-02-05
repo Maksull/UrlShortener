9d4940f340b1940d8c6b5fcf9909b94388bd3022
Maksull <kyrkyrkyr99@gmail.com>
2024-02-05 20:26:05 +0200

    Add exception handler

---
+++ UrlShortenerApi/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortenerApi.Middlewares;
+
+public sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError("An unhandled exception occurred. Exception: {exception}", exception.ToString());
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        ProblemDetails problemDetails = new()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Type = "Server error",
+            Title = "Server error",
+            Detail = "An internal server error has occurred"
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+
+}
