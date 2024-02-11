cb432e249cbdab2a7d9ea561db7cbea5ea13c089
Maksull <kyrkyrkyr99@gmail.com>
2024-02-11 17:12:26 +0200

    Prolong url expiration when client tries to shorten existing url

---
+++ Core/Mediatr/Commands/ProlongUrlExpirationCommand.cs
@@ -0,0 +1,6 @@
+﻿using Core.Entities;
+using MediatR;
+
+namespace Core.Mediatr.Commands;
+
+public sealed record ProlongUrlExpirationCommand(long Id, TimeSpan? ProlongFor = null) : IRequest<Url?>;
---
+++ Infrastructure/Mediatr/Handlers/Urls/ProlongUrlExpirationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Core.Mediatr.Commands;
+using Infrastructure.Data;
+using MediatR;
+
+namespace Infrastructure.Mediatr.Handlers.Urls;
+
+public sealed class ProlongUrlExpirationHandler : IRequestHandler<ProlongUrlExpirationCommand, Url?>
+{
+    private readonly ApiDataContext _apiDataContext;
+
+    public ProlongUrlExpirationHandler(ApiDataContext apiDataContext)
+    {
+        _apiDataContext = apiDataContext;
+    }
+
+    public async Task<Url?> Handle(ProlongUrlExpirationCommand request, CancellationToken cancellationToken)
+    {
+        var url = await _apiDataContext.Urls.FindAsync(request.Id, cancellationToken);
+
+        if (url is null)
+            return url;
+
+        url.ExpireAt = DateTime.UtcNow.Add(request.ProlongFor ?? TimeSpan.FromMinutes(5));
+
+        _apiDataContext.Urls.Update(url);
+
+        await _apiDataContext.SaveChangesAsync(cancellationToken);
+
+        return url;
+    }
+}
